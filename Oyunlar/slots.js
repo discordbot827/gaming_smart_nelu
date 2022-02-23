189d58f0e75484035223c59d5be6156f89f6d2bb
Glitch (pricey-chartreuse-meal) <none>
2022-02-23 19:56:53 +0000

    🎿🍲 Checkpoint ./Oyunlar/slots.js:27258535/7666

--- Oyunlar/slots.js
+++ Oyunlar/slots.js
@@ -8,24 +8,24 @@ exports.run = function(client, message,  args) {
   if(!miktar) return message.channel.send(new Discord.MessageEmbed()
       .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
       .setColor("RANDOM")
-      .setDescription(`**slots** You Can't More Than 💵 50,000`))
+      .setDescription(`**slots** You Can't More Than <a:emoji_50:872142301789564969> 50,000`))
   //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 if(miktar > 50000) return message.channel.send(new Discord.MessageEmbed()
       .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
       .setColor("GREEN")
-      .setDescription(`**slots**You Can't More Than 💵 50,000`))
+      .setDescription(`**slots**You Can't More Than <a:emoji_50:872142301789564969> 50,000`))
   /////////////
 if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   .setColor("RANDOM")
   .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-  .setDescription(`In your wallet to place a bet ${para ? "just " + para + '  there is!' : 'you have no money!'}`))
+  .setDescription(`In your wallet to place a bet ${para ? "just " + para + ' <a:emoji_50:872142301789564969> there is!' : 'you have no money!'}`))
   /////////////////
   if(isNaN(miktar) || miktar < 0) return message.channel.send(new Discord.MessageEmbed()
 .setColor("RED")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(` The amount you entered is not a valid number !?`))
- let kazandin = miktar*2   
-  let slots = ["","<:emoji_45:898254686644207616>","<:emoji_44:898254653291102309>","<a:emoji_46:898256438835707964>"];
+ let kazandin = miktar*3   
+  let slots = ["🤖","🌐","🏅","💵"];
   //
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   let result1 = Math.floor((Math.random() * slots.length));
@@ -47,14 +47,14 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   if (slots[result1] === slots[result2] && slots[result2] === slots[result3] ) {
     var text2 = `**
     ___SLOTS___  
-|<a:emoji_7:897976340018515988> <a:emoji_7:897976340018515988> <a:emoji_7:897976340018515988>|${message.author.username}
+|<a:766617203050938398:891462862667644938><a:766617203050938398:891462862667644938><a:766617203050938398:891462862667644938>|${message.author.username}
 |                  |
 |                  |**`;
           message.channel.send(text2).then(msg => {
             setTimeout(() => {
               msg.edit(`**
     ___SLOTS___  
-|${slots[result1]}${slots[result2]}${slots[result3]}${message.author.username}  ${kazandin}+
+|${slots[result1]}${slots[result2]}${slots[result3]}${message.author.username} <a:emoji_11:777481714800656424> ${kazandin}+
 |                  |
 |                  |**`,true);
             }, 3500);
@@ -65,14 +65,14 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   } else {
     var text3 = `**
     ___SLOTS___  
-| |${message.author.username}
+|<a:766617203050938398:891462862667644938><a:766617203050938398:891462862667644938><a:766617203050938398:891462862667644938>|${message.author.username}
 |                  |
 |                  |**`;
           message.channel.send(text3).then(msg => {
             setTimeout(() => {
               msg.edit(`**
     ___SLOTS___  
-|${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username}  ${miktar}-
+|${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username} <a:emoji_14:777483792159866900> ${miktar}-
 |                  |
 |                  |**`, true);
             }, 3500);
